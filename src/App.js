21c763f89d2b6fdf9ed988c824a5b09e2e08aec6
Vittorio <tonibusatta@gmail.com>
2022-08-15 18:05:21 +0400

    set json file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./Assets/Styles//Fonts.css";
 import "./Assets/Styles/Reset.css";
+var data = require("./Assets/data/data.json");
 
 function App() {
+
+
+  console.log(data);
+
   return (
     <div>
     </div>
